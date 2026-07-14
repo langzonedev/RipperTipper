c4817097d6a15fcf86f91ee69153aaaa960ef49c
Greg <202942822+langzonedev@users.noreply.github.com>
2026-07-14 23:03:19 +0930

    Add ripped football RT launcher logo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.langzonedev.rippertipper"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.6.0"
+        versionCode = 9
+        versionName = "0.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
